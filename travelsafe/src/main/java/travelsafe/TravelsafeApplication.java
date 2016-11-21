6ff839a6c995d7c4716e718768ccb1419f46435c
aloha <radomir.aloha@gmail.com>
2016-11-21 17:43:29 +0100

    init app

---
+++ travelsafe/src/main/java/travelsafe/TravelsafeApplication.java
@@ -0,0 +1,12 @@
+package travelsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsafeApplication.class, args);
+	}
+}
