3834b216a04c124341c2d8f76e82bfa4b2417544
aloha <radomir.aloha@gmail.com>
2016-11-22 15:54:31 +0100

    angular cli

---
+++ travelsafe/src/main/webapp/app/app.module.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('travelsafeapp',
+    ['ngResource', 'ui.router', 'angularModalService','ngSanitize'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/home');
+         $stateProvider
+           .state('home', {
+                   url: '/home',
+                   views: {
+                       'content': {
+                           templateUrl: 'home/home.html',
+                           controller: 'HomeController'
+                       },
+                       'navbar':{
+                           templateUrl: 'navbar/navbar.html',
+                           controller: 'NavbarController'
+                       }
+                   }
+               });
+
+    }).run(function($rootScope,  $state) {
+
+ });
---
+++ travelsafe/src/main/webapp/home/home.controller.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('travelsafeapp')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = ['$scope', '$state'];
+
+    function HomeController ($scope, $state) {
+
+
+    }
+})();
