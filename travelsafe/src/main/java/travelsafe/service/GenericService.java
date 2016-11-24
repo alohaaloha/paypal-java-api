55a7557f7b547d014568affb0d2161f5587e3b8d
aloha <radomir.aloha@gmail.com>
2016-11-24 15:00:34 +0100

    Added generic service. Last commit was: Paypal backend functionality added.

---
+++ travelsafe/src/main/java/travelsafe/service/GenericService.java
@@ -0,0 +1,15 @@
+package travelsafe.service;
+
+import java.util.List;
+
+/**
+ * Created by aloha on 24-Nov-16.
+ */
+public interface GenericService<T> {
+
+    /*Add methods here*/
+
+    List<T> getAll();
+
+
+}
--- travelsafe/src/main/java/travelsafe/service/impl/ItemService.java
+++ travelsafe/src/main/java/travelsafe/service/impl/ItemService.java
@@ -1,28 +1,29 @@
 package travelsafe.service.impl;
 
-import org.hibernate.validator.constraints.ScriptAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import travelsafe.model.Item;
 import travelsafe.repository.ItemRepository;
-import travelsafe.service.ItemService;
+import travelsafe.service.GenericService;
 
 import java.util.List;
 
 /**
- * Created by Dorian on 11/22/2016.
+ * Created by aloha on 24-Nov-16.
  */
 @Service
-@Transactional
-public class JpaItemService implements ItemService {
+public class ItemService implements GenericService<Item> {
 
     @Autowired
     private ItemRepository itemRepository;
+
 
     @Override
     public List<Item> getAll() {
         return itemRepository.findAll();
     }
+
+
+
 
 }
