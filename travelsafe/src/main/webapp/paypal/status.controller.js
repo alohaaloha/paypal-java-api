a83119ab97c53c518bdbc4bae8544034d2ff9218
Dorian <theory93rk@gmail.com>
2017-02-14 00:01:46 +0100

    Status html updated

--- travelsafe/src/main/webapp/paypal/status.controller.js
+++ travelsafe/src/main/webapp/paypal/status.controller.js
@@ -5,9 +5,9 @@
         .module('travelsafeapp')
         .controller('StatusController', HomeController);
 
-    HomeController.$inject = ['$scope', '$state', 'StatusService', '$stateParams', '$location'];
+    HomeController.$inject = ['$scope', '$state', 'StatusService', '$stateParams', '$location', 'ItemService','$translate'];
 
-    function HomeController ($scope, $state, StatusService, $stateParams, $location) {
+    function HomeController ($scope, $state, StatusService, $stateParams, $location, ItemService, $translate) {
 
        //-----------------------------------------------------------------------------------------------------------------------
        //RETURN SAMPLE
@@ -20,6 +20,46 @@
        //$location.search().paymentId;
        //$location.search().PayerID;
        //-----------------------------------------------------------------------------------------------------------------------
+
+        initData();
+
+        function initData(){
+            ItemService.getItemByTypeOfRiskCode("insurance_desc_hi",function(data){
+                $scope.homeInsuranceDesc = data.data;
+
+                var map = {};
+
+                for(var i = 0; i < $scope.homeInsuranceDesc.length; i++){
+                    map[$scope.homeInsuranceDesc[i].id] = false;
+                    if ($translate.use() == "sr")
+                        $scope.homeInsuranceDesc[i].text = $scope.homeInsuranceDesc[i].name_srb;
+                    else if ($translate.use() == "en")
+                        $scope.homeInsuranceDesc[i].text = $scope.homeInsuranceDesc[i].name_en;
+                }
+
+                $scope.insuranceDescResults = map;
+            },function(){
+                console.log("Unsuccessful try to get home insurance risk items.");
+            });
+
+            ItemService.getItemByTypeOfRiskCode("car_package_ci", function (data) {
+                $scope.carPackages = data.data;
+
+                var map = {};
+
+                for(var i = 0; i < $scope.carPackages.length; i++){
+                    map[$scope.carPackages[i].id] = false;
+                    if ($translate.use() == "sr")
+                        $scope.carPackages[i].text = $scope.carPackages[i].name_srb;
+                    else if ($translate.use() == "en")
+                        $scope.carPackages[i].text = $scope.carPackages[i].name_en;
+                }
+
+                $scope.packagesResults = map;
+            }, function () {
+                console.log("Unsuccessful try to get car package risks.");
+            });
+        }
 
        //what to be shown - defalt: cancel page
        $scope.showValue=0;
@@ -35,6 +75,35 @@
                 function(res){
                     $scope.travelInsurance = res.data;
                     console.log($scope.travelInsurance);
+
+                    if(res.data.homeInsurances != null) {
+                        if(res.data.homeInsurances.length != 0) {
+                            for (var i = 0; i < res.data.homeInsurances[0].insuranceDescriptions.length; i++) {
+                                $scope.insuranceDescResults[res.data.homeInsurances[0].insuranceDescriptions[i].id] = true;
+                            }
+                        }
+                    }
+
+                    if(res.data.carInsurances != null){
+                        if(res.data.carInsurances.length != 0){
+                            for(var i = 0; i < res.data.carInsurances[0].carPackagesItems.length; i++){
+                                $scope.packagesResults[res.data.carInsurances[0].carPackagesItems[i].id] = true;
+                            }
+                        }
+                    }
+
+                    $scope.filteredParticipants = [];
+
+                    for(var i = 0; i < res.data.participantInInsurances.length; i++){
+                        var flag = false;
+                        for(var j = 0; j < $scope.filteredParticipants.length; j++){
+                            if(res.data.participantInInsurances[i].id == $scope.filteredParticipants[j].id)
+                                flag = true;
+                        }
+                        if(!flag)
+                            $scope.filteredParticipants.push(res.data.participantInInsurances[i]);
+                    }
+
                 },
                 function(res){
 
